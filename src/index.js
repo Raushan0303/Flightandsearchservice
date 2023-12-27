175e0329cffb789ad1df69a3d8f3388fe122c2b9
Raushan0303 <110516637+Raushan0303@users.noreply.github.com>
2023-12-27 16:55:56 +0530

    do some change

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
+const {PORT} = require('./config/serverConfig');
+
+
 
 const setupAndStartServer = () =>{
 
     const app = express();
-    const PORT = 5047;
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}))
 
     app.listen(PORT, ()=>{
         console.log(`server started at ${PORT}`);
