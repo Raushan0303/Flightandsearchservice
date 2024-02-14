fdf7e8bc675e30eb8f3f49f62575553d974ea480
Raushan0303 <110516637+Raushan0303@users.noreply.github.com>
2024-02-14 17:23:13 +0530

    add repository layer of airport

---
+++ src/repository/airport-repository.js
@@ -0,0 +1,64 @@
+const {Airport} = require('../models/airport');
+const { Op } = require("sequelize");
+
+class AirportRepository{
+    async createAirport(
+        {
+            name,
+            adress,
+            cityId
+        }){
+            try {
+                const airport = await Airport.create({
+                    name,
+                    adress,
+                    cityId
+                });
+                return airport;
+            } catch (error) {
+                console.log("An error occured: at repository layer",error);
+            }
+
+        }
+        async deleteAirport(airportId){
+                try {
+                    const airport = await Airport.destroy({
+                       airportId
+                    });
+                    return airport;
+                } catch (error) {
+                    console.log("An error occured: at repository layer",error);
+                }
+    
+            }
+        async updateAirport(data,airportId){
+            try {
+                const airport = await Airport.create(data,{
+                    where:{
+                        id: airportId
+                    }
+            });
+                return airport;
+            } catch (error) {
+                console.log("An error occured: at repository layer",error);
+            }
+
+        }
+        async getAirport(airportId){
+            try {
+                const airport = await Airport.findByPk(airportId);
+                return airport;
+            } catch (error) {
+                console.log("An error occured: at repository layer",error);
+            }
+    }
+    async getAirport(){
+        try {
+            const airport = await Airport.findAll();
+            return airport;
+        } catch (error) {
+            console.log("An error occured: at repository layer",error);
+        }
+}
+      
+}
